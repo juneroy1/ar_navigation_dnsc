5e14895eb0d5c2f17202deb2e672a7a5b5354567
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2024-07-27 23:49:49 +0800

    created new route for deleting announcement

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::get('/', 'AdminController@index')->name('admin');
 Route::get('/admin', 'AdminController@index')->name('admin');
 Route::post('/announcement', 'AdminController@createAnnouncement')->name('createAnnouncement');
 Route::get('/announcement', 'AdminController@announcement')->name('announcement');
+Route::get('/deleteAnnouncement/{id}', 'AdminController@deleteAnnouncement')->name('deleteAnnouncement');
 
 Route::get('/lost-and-found', 'AdminController@lost_and_found')->name('lost_and_found');
 Route::get('/user', 'AdminController@index')->name('user');
