cc2e11f87780c5b14a6c3f9e223d9bbe4c5cc020
Juneroy Quinimon <eroyjune@gmail.com>
2024-07-28 01:00:37 +0800

    created new function for lost and found create new

--- app/Http/Controllers/LostAndFoundController.php
+++ app/Http/Controllers/LostAndFoundController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\LostAndFound;
 
 class LostAndFoundController extends Controller
 {
     //
+    public function createLostAndFound(Request $request)
+    {
+        $user = Auth::user();
+        $id = Auth::id();
+
+        $create = new LostAndFound();
+        $create->title = $request->title;
+        $create->description = $request->description;
+        $create->image = "";
+        $create->user_id = $id;
+        $create->save();
+        return redirect()->back()->with('success', 'Successfully created new lost and found');   
+
+        
+    }
 }
