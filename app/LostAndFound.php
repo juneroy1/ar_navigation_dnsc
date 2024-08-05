96c575242459c124c0ab62e19b17e71e75301567
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-06 01:15:12 +0800

    feat: added new field to allow read in LAF model

--- app/LostAndFound.php
+++ app/LostAndFound.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class LostAndFound extends Model
 {
     //
-    protected $fillable = ['title', 'description', 'image',  'user_id'];
+    protected $fillable = ['title', 'description', 'image',  'user_id', 'name', 'avatar'];
     protected $table = "lost_and_found";
 
 }
