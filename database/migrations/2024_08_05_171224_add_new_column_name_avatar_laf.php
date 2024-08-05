846d7b06a00e06d53ef3fcf8604506ee3c80d26e
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-06 01:14:52 +0800

    feat: added new migration for adding name and avatar in LAF

---
+++ database/migrations/2024_08_05_171224_add_new_column_name_avatar_laf.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnNameAvatarLaf extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lost_and_found', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lost_and_found', function (Blueprint $table) {
+            //
+        });
+    }
+}
