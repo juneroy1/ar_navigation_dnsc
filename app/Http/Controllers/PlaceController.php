874686b91ee2266e00386936e65372b457303cc4
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-02 17:47:13 +0800

    feat: added function for getting the list of places

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Model\PlaceModel;
 class PlaceController extends Controller
 {
     /**
@@ -14,6 +14,19 @@ class PlaceController extends Controller
     public function index()
     {
         //
+
+         $user = Auth::user();
+        $id = Auth::id();
+        
+        $places = PlaceModel::all();
+      
+            return view('admin.places', [
+                'pageName' => 'places',
+                'update'=> false,
+                'edit' => false,
+
+                'places' => $places
+            ]);
     }
 
     /**
