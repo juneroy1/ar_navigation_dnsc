1177960396bbf14eb2fa99cfbadac5c6bfa5785b
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-03 17:48:56 +0800

    feaT: added relationship for place to destination by using place_id_from and place_id_to

--- app/PlaceModel.php
+++ app/PlaceModel.php
@@ -3,10 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\DestinationModel;
 class PlaceModel extends Model
 {
     //
     protected $fillable = ['name','user_id'];
     protected $table = "places";
+
+     public function destinations_from()
+    {
+        return $this->hasMany(DestinationModel::class,'place_id_from');
+    }
+
+     public function destinations_to()
+    {
+        return $this->hasMany(DestinationModel::class,'place_id_to');
+    }
 }
