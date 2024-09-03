f837e52cf5e504dca67f3ae4aed60077c409a71f
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-03 17:14:41 +0800

    feat: added fillable fields for destination

--- app/DestinationModel.php
+++ app/DestinationModel.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 class DestinationModel extends Model
 {
     //
-    protected $fillable = ['name'];
+    protected $fillable = ['place_id_from', 'place_id_to'];
     protected $table = "destinations";
 }
