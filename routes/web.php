b2a8feaca87e6b90e53a00a0d12431537e031994
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-03 17:37:56 +0800

    feat: created new route api for creating new destination

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/deletePlace/{id}', 'PlaceController@destroy')->name('delete_place')
 
 // destination
 Route::get('/destinations', 'DestinationController@index')->name('get_destinations');
+Route::post('/destination', 'DestinationController@store')->name('create_destination');
 
 Route::post('/announcement', 'AdminController@createAnnouncement')->name('createAnnouncement');
 Route::get('/announcement', 'AdminController@announcement')->name('announcement');
