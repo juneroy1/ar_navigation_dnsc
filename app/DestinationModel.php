19e2fa47bf3b32fd38ebac5ac10b2a441c615eb6
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-03 17:47:35 +0800

    feat: added relation for destination on places with not using id but using unique id from the parent table

--- app/DestinationModel.php
+++ app/DestinationModel.php
@@ -2,10 +2,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\PlaceModel;
 class DestinationModel extends Model
 {
     //
     protected $fillable = ['place_id_from', 'place_id_to'];
     protected $table = "destinations";
+
+
+     public function place()
+    {
+        return $this->belongsTo(PlaceModel::class, 'place_id_from', 'id');
+    }
 }
