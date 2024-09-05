879f2ef2ba29ab24acbba213e23f7320c59a470f
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-05 23:26:51 +0800

    feat: changed auth type for the admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['api']);
     }
     /**
      * Display a listing of the resource.
