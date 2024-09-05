a30c35383a0bf218514c04c032f28315fb4cf1b3
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-05 23:28:16 +0800

    feat: changed api auth type for destinationController

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -11,7 +11,7 @@ class DestinationController extends Controller
 {
       public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('api');
     }
     /**
      * Display a listing of the resource.
