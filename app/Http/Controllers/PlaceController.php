b6e4f7d5a6b484ec4271541655d0a64ea97ff005
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-08 21:55:22 +0800

    feat change auth type for place controller

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -10,7 +10,7 @@ class PlaceController extends Controller
 {
       public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('api');
     }
     /**
      * Display a listing of the resource.
