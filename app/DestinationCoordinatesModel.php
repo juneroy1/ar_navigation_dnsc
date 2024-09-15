7008be41a81e960a4f4f3b2ffa5debf1008777a6
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-15 22:15:27 +0800

    feat added fillable fields for destination coordinates table

--- app/DestinationCoordinatesModel.php
+++ app/DestinationCoordinatesModel.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class DestinationCoordinatesModel extends Model
 {
     //
+    protected $fillable = ['x', 'y', 'z', 'destination_id', 'time', 'longitude', 'latitude'];
 }
