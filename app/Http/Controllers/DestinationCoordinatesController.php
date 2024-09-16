be7c9e45e0561ada164c8d0498e48be10ccc8afc
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-16 23:07:05 +0800

    feat added construct function for DestinationCoordinatesController

--- app/Http/Controllers/DestinationCoordinatesController.php
+++ app/Http/Controllers/DestinationCoordinatesController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class DestinationCoordinatesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
     /**
      * Display a listing of the resource.
      *
